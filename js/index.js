2a369940195365eb2cc375d2000b536a7bbae685
Auezov <jasonadal354@gmail.com>
2023-02-02 18:45:28 -0500

    third person

--- js/index.js
+++ js/index.js
@@ -10,3 +10,23 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// Add functionality for a dark and light mode switch: 
+// Use .addEventListener to set up an element to listen for an event: 
+// Need to add HTML, make a switch underneath the '{JSONadalwhite}' logo
+
+const themeSwitcher = docuement.querySelector("");
+const container = document.querySelector("");
+
+const mode = "dark";
+
+themeSwitcher.addEventListener("click", function() {
+    if(mode === "dark") {
+        mode = "light";
+        container.setAttribute("class", "light");
+    }
+    else{
+        mode = "dark";
+        container.setAttribute("class", "dark");
+    }
+});
